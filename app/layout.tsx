220206c1a26f4b907b371fa27dd77028621ac72d
Emanuel Tun Poot <ETPoot@aitulum.mx>
2025-08-09 18:20:42 -0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="es">
+      <body className={`${inter.className} antialiased`}>
+         <h1 className='text-blue-500'>Prueba Tailwind</h1>
+           {children}
+        </body>
     </html>
   );
 }
